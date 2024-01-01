b2bebfec8c53ce96b79bdb495ec27adfda376b40
smit vekariya <smit vekariya>
2024-01-01 19:33:01 +0000

    new commit3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,6 +20,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -34,7 +35,9 @@ SECRET_KEY = 'django-insecure-di-p=wlzad=(z38btnh0fr+60=3cz#h!9q(&f_69d7*ojzqu82
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+
+ALLOWED_HOSTS = ["panelprime.pythonanywhere.com","*"]
+
 
 AUTH_USER_MODEL = 'account.BondUser'
 AUTHENTICATION_BACKENDS = ['account.backends.MobileNumberBackend','django.contrib.auth.backends.ModelBackend']
@@ -64,6 +67,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -147,8 +151,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = env("STATIC_URL")
-STATIC_ROOT = env("STATIC_ROOT")
+# STATIC_URL = env("STATIC_URL")
+# STATIC_ROOT = env("STATIC_ROOT")
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
