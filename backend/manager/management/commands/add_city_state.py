f28a9f8ba374cfd583089b18dc22baa9f167421c
smit <smitvekariya164@gmail.com>
2024-01-09 00:57:43 +0530

    Update add_city_state.py

--- backend/manager/management/commands/add_city_state.py
+++ backend/manager/management/commands/add_city_state.py
@@ -10,10 +10,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             with transaction.atomic():
-                df = pd.read_excel(str(settings.BASE_DIR) +'\manager\Files\Districts.xlsx')
+                df = pd.read_excel(str(settings.BASE_DIR) +'/manager/files/Districts.xlsx')
                 for index, row in df.iterrows():
                     data,response = State.objects.get_or_create(name=row['State'])
-                    City.objects.create(name=row['City'],state_id=data.id)
+                    City.objects.get_or_create(name=row['City'],state_id=data.id)
                 print("City and state add successfully.")
         except Exception as e:
             manager.create_from_exception(e)
