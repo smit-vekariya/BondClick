7fe158206e375b07542462038efeadf387066e7a
smit vekariya <smit vekariya>
2024-02-03 10:30:25 +0000

    change settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,14 +97,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database (https://docs.djangoproject.com/en/4.2/ref/settings/#databases)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {"default": {
-    "ENGINE": env("ENGINE"),
-    "NAME": env("NAME"),
-    "USER": env("USER"),
-    "PASSWORD": env("PASSWORD"),
-    "HOST": env("HOST"),
-    "PORT": env("PORT")}
+    "ENGINE": os.environ.get("ENGINE", 'django.db.backends.sqlite3'),
+    "NAME": os.environ.get("NAME", str(BASE_DIR / 'db.sqlite3')),
+    "USER": os.environ.get("USER",''),
+    "PASSWORD": os.environ.get("PASSWORD",''),
+    "HOST": os.environ.get("HOST",''),
+    "PORT": os.environ.get("PORT",'')}
 }
 
 # Password validation (https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators)
